fe3233fbd90fd23ede76586f96cc721a77860300
Gurukeerthana11 <96216442+Gurukeerthana11@users.noreply.github.com>
2025-03-17 15:44:37 +0530

    Update SQLQuery1.sql

--- ASSIGNMENT 1/SQLQuery1.sql
+++ ASSIGNMENT 1/SQLQuery1.sql
@@ -26,7 +26,7 @@ create table projects (
     start_date date,
     planned_end_date date,
     actual_end_date date ,
-    budget decimal(12, 2) check (budget > 0),
+    budget int,
     client_id int,
     constraint fk_client foreign key (client_id) references clients(client_id)
 );
@@ -51,22 +51,22 @@ insert into departments values (20, 'Development', 'Pune');
 insert into departments values (30, 'Testing', 'Mumbai');
 insert into departments values (40, 'Document', 'Mumbai');
 
-insert into employees values (7001, 'Sandeep', 'Analyst', 25000.00, 10);
-insert into employees values (7002, 'Rajesh', 'Designer', 30000.00, 10);
-insert into employees values (7003, 'Madhav', 'Developer', 40000.00, 20);
-insert into employees values (7004, 'Manoj', 'Developer', 40000.00, 20);
-insert into employees values (7005, 'Abhay', 'Designer', 35000.00, 10);
-insert into employees values (7006, 'Uma', 'Tester', 30000.00, 30);
-insert into employees values (7007, 'Gita', 'Tech. Writer', 30000.00, 40);
-insert into employees values (7008, 'Priya', 'Tester', 35000.00, 30);
-insert into employees values (7009, 'Nutan', 'Developer', 45000.00, 20);
-insert into employees values (7010, 'Smita', 'Analyst', 20000.00, 10);
-insert into employees values (7011, 'Anand', 'Project Mgr', 65000.00, 10);
+insert into employees values (7001, 'Sandeep', 'Analyst', 25000, 10);
+insert into employees values (7002, 'Rajesh', 'Designer', 30000, 10);
+insert into employees values (7003, 'Madhav', 'Developer', 40000, 20);
+insert into employees values (7004, 'Manoj', 'Developer', 40000, 20);
+insert into employees values (7005, 'Abhay', 'Designer', 35000, 10);
+insert into employees values (7006, 'Uma', 'Tester', 30000, 30);
+insert into employees values (7007, 'Gita', 'Tech. Writer', 30000, 40);
+insert into employees values (7008, 'Priya', 'Tester', 35000, 30);
+insert into employees values (7009, 'Nutan', 'Developer', 45000, 20);
+insert into employees values (7010, 'Smita', 'Analyst', 20000, 10);
+insert into employees values (7011, 'Anand', 'Project Mgr', 65000, 10);
 
-insert into projects values (401, 'Inventory', '2011-04-01', '2011-10-01', '2011-10-31', 150000.00, 1001);
-insert into projects values (402, 'Accounting', '2011-08-01', '2012-01-01', null, 500000.00, 1002);
-insert into projects values (403, 'Payroll', '2011-10-01', '2011-12-31', null, 75000.00, 1003);
-insert into projects values (404, 'Contact Mgmt', '2011-11-01', '2011-12-31', null, 50000.00, 1004);
+insert into projects values (401, 'Inventory', '2011-04-01', '2011-10-01', '2011-10-31', 150000, 1001);
+insert into projects values (402, 'Accounting', '2011-08-01', '2012-01-01', null, 500000, 1002);
+insert into projects values (403, 'Payroll', '2011-10-01', '2011-12-31', null, 75000, 1003);
+insert into projects values (404, 'Contact Mgmt', '2011-11-01', '2011-12-31', null, 50000, 1004);
 
 insert into empprojecttasks values (401, 7001, '2011-04-01', '2011-04-20', 'System Analysis', 'Completed');
 insert into empprojecttasks values (401, 7002, '2011-04-21', '2011-05-30', 'System Design', 'Completed');
