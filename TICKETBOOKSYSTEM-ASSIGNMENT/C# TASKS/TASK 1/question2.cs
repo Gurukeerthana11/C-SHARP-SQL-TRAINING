1f5e3843a3f713b45e230ad58839e3b6d80956e1
Gurukeerthana11 <96216442+Gurukeerthana11@users.noreply.github.com>
2025-04-08 18:30:12 +0530

    Add files via upload

---
+++ TICKETBOOKSYSTEM-ASSIGNMENT/C# TASKS/TASK 1/question2.cs
@@ -0,0 +1,17 @@
+﻿namespace question2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter available tickets : ");
+            int availableticket = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter No of tickets : ");
+            int no_of_bookings = Convert.ToInt32(Console.ReadLine());
+            if(availableticket >= no_of_bookings)
+                Console.Write("The ticket is available");
+            else
+                Console.Write("The ticket is not available");
+        }
+    }
+}
---
+++ TICKETBOOKSYSTEM-ASSIGNMENT/C# TASKS/TASK 1/question3.cs
@@ -0,0 +1,20 @@
+﻿namespace question3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter available tickets : ");
+            int availableticket = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter No of tickets : ");
+            int no_of_bookings = Convert.ToInt32(Console.ReadLine());
+            if (availableticket >= no_of_bookings)
+            {
+                
+                Console.Write("The ticket is available");
+            }
+            else
+                Console.Write($"The ticket is not available.{no_of_bookings-availableticket} tickets are more from available tickets" );
+        }
+    }
+}
