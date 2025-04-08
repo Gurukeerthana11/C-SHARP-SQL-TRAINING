d4edba21ac09ed7dc2899c68e69f14c8d5e3d6b4
Gurukeerthana11 <96216442+Gurukeerthana11@users.noreply.github.com>
2025-04-08 19:07:35 +0530

    Add files via upload

---
+++ TICKETBOOKSYSTEM-ASSIGNMENT/C# TASKS/TASK2/question1.cs
@@ -0,0 +1,55 @@
+﻿/*Task 2: Nested Conditional Statements 
+Create a program that simulates a Ticket booking and calculating cost of tickets. Display tickets options 
+such as "Silver", "Gold", "Dimond". Based on ticket category fix the base ticket price and get the user input 
+for ticket type and no of tickets need and calculate the total cost of tickets booked.*/
+
+namespace question1
+{
+   
+
+    class TicketBooking
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to Ticket Booking System");
+            Console.WriteLine("Available Ticket Categories:");
+            Console.WriteLine("1. Silver");
+            Console.WriteLine("2. Gold");
+            Console.WriteLine("3. Diamond");
+
+            Console.Write("Enter your ticket category (Silver/Gold/Diamond): ");
+            string ticketType = Console.ReadLine();
+
+            Console.Write("Enter number of tickets: ");
+            int numberOfTickets= Convert.ToInt32(Console.ReadLine());
+            int pricePerTicket = 0;
+
+            if (ticketType == "silver")
+            {
+                pricePerTicket = 100;
+            }
+            else if (ticketType == "gold")
+            {
+                pricePerTicket = 200;
+            }
+            else if (ticketType == "diamond")
+            {
+                pricePerTicket = 300;
+            }
+            else
+            {
+                Console.WriteLine("Invalid ticket category selected.");
+                return;
+            }
+
+            int totalCost = pricePerTicket * numberOfTickets;
+
+            Console.WriteLine("\nBooking Summary:");
+            Console.WriteLine($"Ticket Type: {ticketType.ToUpper()}");
+            Console.WriteLine($"Number of Tickets: {numberOfTickets}");
+            Console.WriteLine($"Price per Ticket: Rs.{pricePerTicket}");
+            Console.WriteLine($"Total Cost: Rs.{totalCost}");
+        }
+    }
+
+}
