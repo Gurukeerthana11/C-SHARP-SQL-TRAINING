0c937746b77e5332f6bc1c31c61dc0d0ebffa6eb
Gurukeerthana11 <96216442+Gurukeerthana11@users.noreply.github.com>
2025-04-08 17:17:00 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace question1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter available tickets : ");
+            int availableticket=Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter No of tickets : ");
+            int no_of_bookings=Convert.ToInt32(Console.ReadLine());
+            //to display
+            //Console.WriteLine($"availableticket is {availableticket}\nNo of bookings is {no_of_bookings}");
+        }
+    }
+}
